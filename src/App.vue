cc4c500863f920f038c17954f6b48513e2d2a654
Anant Mathur <anant.mathur007@gmail.com>
2021-06-20 16:22:12 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<LearningResource v-for="res in storedResources" :key="res.id" />
+  <ul></ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+  components: {LearningResource};
+  data() {
+    return {
+      storedResources: [
+        {
+          id: '1',
+          title: 'Official Guide',
+          description: 'This is the official Vue js doc',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: '2',
+          title: 'Google',
+          description: 'This is the official google doc',
+          link: 'https://google.com'
+        }
+      ]
+    };
+  }
+};
+</script>
